46f2f9e2774e61d09241b7d5e85175d1bb7e83ac
Devanand <devanandb@darkVulcan.local>
2019-06-29 14:26:21 +0530

    fix:Fix the build issue for missing image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,19 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Jim Nieters</h1>
+    {/* <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p> */}
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      {/* <Image /> */}
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="content/experience-led-differentiating-on-design">Experience-Led: Differentiating on Design</Link>
   </Layout>
 )
 

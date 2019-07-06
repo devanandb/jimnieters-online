ffe5980cc14990eb41120f62cdd0c8bbe163e499
Devanand <dev.ryder88@gmail.com>
2019-07-06 18:30:23 +0530

    build:Add netlify caching for build optimize

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,6 +86,7 @@ module.exports = {
         minify: true,
       },
     },
+    'gatsby-plugin-netlify-cache',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
